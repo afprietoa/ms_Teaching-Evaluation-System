81afeab1522fe5a9aa0f7109e774fec3d685dee8
Andres Prieto <afprietoa@unal.edu.co>
2023-06-11 17:12:24 -0500

    initial configuration

---
+++ src/main/java/com/unal/TeachingEvaluation/TeachingEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.unal.TeachingEvaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unal/TeachingEvaluation/TeachingEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unal.TeachingEvaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
